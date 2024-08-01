c6de46fb90efa3162891805dc4e26d27418adf2f
aryangavit <aryan.code01@gmail.com>
2024-08-02 00:35:30 +0530

    pages and route setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+//page components
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import Search from './pages/search/Search'
+import Recipe from './pages/recipe/Recipe'
 
 function App() {
   return (
     <div className="App">
-      <p>hello</p>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/create">
+            <Create/>
+          </Route>
+          <Route path="/search">
+            <Search/>
+          </Route>
+          <Route path="/recipe/:id">
+            <Recipe/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
